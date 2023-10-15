058cba551aa58801460ceb7e07d949965d311d58
Victor He <vvhe040265@gmail.com>
2023-10-14 20:30:13 -0500

    .env add

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
   user: "postgres",
-  password: "Helimon13$",
+  password: process.env.POOL_PASSWORD,
   host: "localhost",
   port: 5432,
   database: "summoners",
