2cc59e286b893113d98413f39b688da59fc0c5c1
Victor He <vvhe040265@gmail.com>
2023-10-14 23:54:09 -0500

    working...

--- server/db.js
+++ server/db.js
@@ -2,12 +2,12 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: process.env.POOL_PASSWORD,
-  host: "localhost",
+  user: "summoners_user",
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
   port: 5432,
   database: "summoners",
+  ssl: true,
 });
-
 
 module.exports = pool;
